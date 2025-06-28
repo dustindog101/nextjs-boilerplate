930e52c237bcb38d5476fda3fb75d5fa5074db24
Dustin Haile <social@cybershare.tech>
2025-06-28 18:50:09 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -171,7 +171,7 @@ export default function App() {
                         Track
                     </a>
                   
-                    <<a href="/order" className="flex items-center justify-center bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-8 rounded-lg transition-colors duration-300 text-lg shadow-lg hover:shadow-xl transform hover:-translate-y-1">
+                    <a href="/order" className="flex items-center justify-center bg-green-600 hover:bg-green-700 text-white font-bold py-3 px-8 rounded-lg transition-colors duration-300 text-lg shadow-lg hover:shadow-xl transform hover:-translate-y-1">
                         <BoxIcon />
                         Order
                     
